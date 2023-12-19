8968bfc5c3f38f911f6f49956a9c13af6f40f407
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2023-12-18 23:31:01 -0300

    show basic card

--- src/app/gifs/pages/home/home-page.component.ts
+++ src/app/gifs/pages/home/home-page.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+import { Gif } from '../../interfaces/gifs.interfaces';
 
 @Component({
   selector: 'gifs-home-page',
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {
-
+  public constructor(private _gifsService: GifsService) { }
+  
+  public get gifs () : Gif[] {
+    return this._gifsService.gifsList
+  }
 }
